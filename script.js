cfc321d45bf8c4407d6ad7bd92f5adde898a0223
JohnxSnow <xi66ix1@gmail.com>
2026-07-18 21:49:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,24 @@ const giftSection = document.getElementById('gift-section');
 const audioContainer = document.getElementById('audio-container');
 
 // زیادکردنی گۆرانییەکە لێرە
-const song = new Audio("https://www.mboxdrive.com/CMTp8sGQxQI.mp3"); 
+// لێرە تەنها ناوی فایلەکە بنووسە
+const song = new Audio("song.mp3"); 
+
+// ... باقی کۆدەکەت
+
+yesBtn.addEventListener('click', () => {
+    // لێدانی گۆرانییەکە
+    song.play().then(() => {
+        console.log("گۆرانییەکە دەستی پێکرد");
+    }).catch(error => {
+        console.log("پێویستە کلیک بکەیت بۆ ئەوەی گۆرانییەکە دەست پێ بکات");
+    });
+    
+    document.querySelector('.buttons').style.display = 'none';
+    mainImg.style.display = 'none';
+    question.style.display = 'none';
+    giftSection.style.display = 'block';
+});
 
 const messages = ["بڕیارەکەت بگۆڕە! 🥺", "بیرێکی تر بکەوە! 😭", "دڵم مەشکێنە! 💔", "تکایە... 🥀"];
 let index = 0;
